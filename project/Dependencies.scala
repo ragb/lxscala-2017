9562efe195cb5767e7e896cfa25d8fd80e6ab230
Rui Batista <ruiandrebatista@gmail.com>
2017-04-15 19:55:26 +0100

    Bump dependency versions.

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,8 +4,8 @@ import sbt._
 object Dependencies {
   lazy val logback ="ch.qos.logback" % "logback-classic" % logbackVersion
   lazy val scalaLogging = "com.typesafe.scala-logging" %% "scala-logging" % scalaLoggingVersion
-  lazy val http4sBlazeClient = "org.http4s" %% "http4s-blaze-client" % http4sVersion changing()
-  lazy val http4sDsl = "org.http4s" %% "http4s-dsl" % http4sVersion changing()
+  lazy val http4sBlazeClient = "org.http4s" %% "http4s-blaze-client" % http4sVersion
+  lazy val http4sDsl = "org.http4s" %% "http4s-dsl" % http4sVersion
   lazy val fs2core = "co.fs2" %% "fs2-core" % fs2version
   lazy val jawnFs2 = "org.http4s" %% "jawn-fs2" % jawnFs2version
   lazy val fs2Kafka = "co.enear" %% "fs2-kafka" % fs2KafkaVersion
@@ -30,14 +30,14 @@ lazy val macroParadise = compilerPlugin("org.scalamacros" % "paradise" % "2.1.0"
 // Library versions
   val logbackVersion = "1.2.1"
   val scalaLoggingVersion = "3.5.0"
-  val http4sVersion = "0.17.0-SNAPSHOT"
+  val http4sVersion = "0.17.0-M1"
   val circeVersion = "0.7.0"
-  val fs2version = "0.9.4"
+  val fs2version = "0.9.5"
   val jawnFs2version = "0.10.1"
   val specs2Version = "3.8.9"
   val pureconfigVersion = "0.6.0"
   val grafterVersion = "1.4.8"
-  val fs2KafkaVersion = "0.0.1"
+  val fs2KafkaVersion = "0.0.2"
   val revealjsVersion = "3.1.0"
   val catsVersion = "0.9.0"
 
